c62b14918ed4f9fdd7ec67785bddcc26097f88d0
asyncedd <isynqquwu@proton.me>
2023-05-14 07:59:25 +0900

    feat(wezterm): ✨ Render the tab bar at the bottom

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,8 @@ config.font = wezterm.font("Cartograph CF")
 
 config.use_fancy_tab_bar = false
 
+config.tab_bar_at_bottom = true
+
 wezterm.on("format-tab-title", function(tab)
   local prog = tab.active_pane.user_vars.PROG
   return tab.active_pane.title .. (prog or "")
