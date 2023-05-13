e4c75f23bda0c482d0377eb119aa9a1cfb977aeb
asyncedd <isynqquwu@proton.me>
2023-05-14 07:55:41 +0900

    feat(wezterm): ✨ Use the retro bar 🏙️

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,8 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("Cartograph CF")
 
+config.use_fancy_tab_bar = false
+
 wezterm.on("format-tab-title", function(tab)
   local prog = tab.active_pane.user_vars.PROG
   return tab.active_pane.title .. (prog or "")
