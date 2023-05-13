9ad5cfe13effea5dbaeda4d80fbe65565d31d0d5
asyncedd <isynqquwu@proton.me>
2023-05-14 07:29:24 +0900

    feat(wezterm): ✨ Make tabs show relevant shell information

--- wezterm.lua
+++ wezterm.lua
@@ -6,4 +6,9 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("Cartograph CF")
 
+wezterm.on("format-tab-title", function(tab)
+  local prog = tab.active_pane.user_vars.PROG
+  return tab.active_pane.title .. (prog or "")
+end)
+
 return config
