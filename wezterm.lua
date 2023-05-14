533ff5ae626d67e86bb26aaf9a6bc552c4ec3d81
asyncedd <isynqquwu@proton.me>
2023-05-14 09:01:46 +0900

    feat(tabline): ✨ Customized it

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require("wezterm")
 
+local palette = require("wezterm.palettes")
+
 local config = {}
 
 config.color_scheme = "Catppuccin Mocha"
@@ -10,9 +12,41 @@ config.use_fancy_tab_bar = false
 
 config.tab_bar_at_bottom = true
 
+config.colors = {
+  tab_bar = {
+    background = palette.mantle,
+    active_tab = {
+      bg_color = palette.mauve,
+      fg_color = palette.mantle,
+      intensity = "Bold",
+      underline = "None",
+      italic = false,
+      strikethrough = false,
+    },
+    inactive_tab = {
+      bg_color = palette.mantle,
+      fg_color = palette.surface1,
+    },
+    inactive_tab_hover = {
+      bg_color = palette.mantle,
+      fg_color = palette.surface1,
+    },
+    new_tab = {
+      bg_color = palette.mantle,
+      fg_color = palette.subtext0,
+    },
+    new_tab_hover = {
+      bg_color = palette.mantle,
+      fg_color = palette.surface2,
+    },
+  },
+}
+
 wezterm.on("format-tab-title", function(tab)
   local prog = tab.active_pane.user_vars.PROG
   return tab.active_pane.title .. (prog or "")
 end)
+
+require("wezterm.tab")
 
 return config
