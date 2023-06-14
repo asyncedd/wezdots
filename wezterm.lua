88da2a59ab3a37dd90ba6278099d95663c6add6e
asyncedd <isynqquwu@proton.me>
2023-06-14 15:57:37 +0900

    feat(wezterm): initial commit

--- wezterm.lua
+++ wezterm.lua
@@ -2,13 +2,13 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.color_scheme = "Catppuccin Mocha"
-
 config.font = wezterm.font("Cartograph CF")
+
+config.color_scheme = "Catppuccin Mocha"
 
 config.animation_fps = 1
 
-config.cursor_blink_rate = 0
-
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 return config
